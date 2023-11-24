64de4c92ae0791cd8da7be44053dce3fab001b45
Andy Braga <bragovo@gmail.com>
2023-11-24 23:19:58 +0300

    Add prefix

--- src/app.rs
+++ src/app.rs
@@ -4,7 +4,7 @@ use axum::{routing::get, Router};
 use tracing::info;
 
 pub async fn run() {
-    let app = Router::new().route("/healthz", get(|| async {}));
+    let app = Router::new().route("/api/healthz", get(|| async {}));
 
     let addr = &env::var("APP_ADDR").unwrap();
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
