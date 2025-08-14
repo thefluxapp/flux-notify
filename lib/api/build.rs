d10ebdb5b9e1a882bb60c81750b3baaad50595cb
Andy Braga <bragovo@gmail.com>
2025-08-14 17:45:05 +0300

    Bump prost & tonic (#6)

--- lib/api/build.rs
+++ lib/api/build.rs
@@ -3,12 +3,12 @@ use std::{env, path::PathBuf};
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_server(false)
         .compile_protos(&["src/events.proto"], &["src"])
         .unwrap();
 
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .file_descriptor_set_path(out_dir.join("pushes_descriptor.bin"))
         .compile_protos(&["src/pushes.proto"], &["src"])
         .unwrap();
